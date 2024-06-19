3b558ad82e65e1290d45ded392e953ed236b0e32
alfredoT7 <123456alfredotorri@gmail.com>
2024-06-19 14:29:28 -0400

    feat: Fist Api using graphene, Studebnt data APi

--- ApiGraphene/schema.py
+++ ApiGraphene/schema.py
@@ -1,7 +1,20 @@
 import graphene 
+from graphene_django import DjangoObjectType
+from usuarios.models import UserN, Student
+
+class StudentType(DjangoObjectType):
+    class Meta:
+        model = Student
+        fields = "__all__"#los campos que quiera
 
 class Query(graphene.ObjectType):
     hello=graphene.String(default_value="Hello!")
+    students = graphene.List(StudentType)
 
+    def resolve_userN(self, info):
+        return UserN.objects.all()
+    
+    def resolve_students(self, info):
+        return Student.objects.all()
     
 schema=graphene.Schema(query=Query)
